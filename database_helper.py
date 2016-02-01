a6cf89a442c6290a13c52f04a1c3a1f0b2ab14f1
Sebastian Wikström <sebwi101@student.liu.se>
2016-02-01 17:02:28 +0100

    Implemented sign in and sign up functions

--- database_helper.py
+++ database_helper.py
@@ -1,22 +1,46 @@
+from flask import Flask
 import sqlite3
 from flask import g
 
+app = Flask(__name__)
+
 DATABASE = 'database.db'
 
-def getDb():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = connect_to_database()
-    return db
+databaseConnection = None
 
-@app.teardown_appcontext
-def closeConnection():
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
+def connectToDatabase():
+    global databaseConnection
+    databaseConnection = sqlite3.connect(DATABASE)
 
-def queryDb(query, args=(), one=False):
-    cursor = getDb().execute(query, args)
-    result = cursor.fetchall()
+def closeDatabaseConnection():
+    if databaseConnection is not None: 
+        databaseConnection.close()
+
+def getUserByPassword(email, passwordHash):
+    cursor = databaseConnection.cursor()
+    cursor.execute('select * from users where email = ? and password = ?', (email, passwordHash))
+    result = cursor.fetchone()
     cursor.close()
-    return (result[0] if result else None) if one else result
+    return result
+
+def getUserByEmail(email):
+    cursor = databaseConnection.cursor()
+    cursor.execute('select * from users where email = ?', (email,))
+    result = cursor.fetchone()
+    cursor.close()
+    return result
+
+def insertSignedInUser(token, email):
+    cursor = databaseConnection.cursor()
+    cursor.execute('insert into signedInUsers values (?, ?)', (token, email))
+    databaseConnection.commit()
+    cursor.close()
+    return True
+
+def insertUser(email, firstName, lastName, gender, city, country, passwordHash):
+    cursor = databaseConnection.cursor()
+    cursor.execute('insert into users values (?, ?, ?, ?, ?, ?, ?)', (email, passwordHash, firstName, lastName, gender, city, country))
+    databaseConnection.commit()
+    cursor.close()
+    return True
+
--- server.py
+++ server.py
@@ -1,37 +1,89 @@
-from __future__ import print_function # In python 2.7
+import json
 from flask import Flask
 from flask import request
-from flask import jsonify
 from flask.ext.bcrypt import Bcrypt
-import sys
-#import database_helper
+from wtforms import Form, TextField, PasswordField, validators
+from random import randint
+
+import database_helper
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
-@app.route('/')
-def home():
-    return 'Hello'
+class SignUpForm(Form):
+    firstName = TextField('First name', [validators.Required()])
+    lastName = TextField('Last name', [validators.Required()])
+    gender = TextField('Gender', [validators.Required()])
+    city = TextField('City', [validators.Required()])
+    country = TextField('Country', [validators.Required()])
+    signupEmail = TextField('Email', [validators.Required(), validators.Email()])
+    signupPassword = PasswordField('Password', [validators.Required(), validators.Length(min=6, max=30), validators.EqualTo('repeatPassword', message='Password doesn\'t match')])
+    repeatPassword = PasswordField('Repeat password', [validators.Required()])
+
+@app.before_request
+def beforeRequest():
+    database_helper.connectToDatabase()
+
+@app.teardown_request
+def teardownRequest(exception):
+    database_helper.closeDatabaseConnection()
 
 def createToken():
     letters = 'abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
     token = ''
     for i in range(0, 36):
-        token += letters[randint(0,len(letters))]
+        token += letters[randint(0,len(letters) - 1)]
     return token
 
 def validLogin(email, password):
-    passwordHash = bcrypt.generate_password_hash(password);
-    user = queryDb('select * from users WHERE email = ? and password = ?', [email, passwordHash], one=True)
-    return user is None
+    user = database_helper.getUserByEmail(email)
+    if user is None:
+        return False
+    else:
+        print user
+        if bcrypt.check_password_hash(user[1], password):
+            return True
+        else:
+            return False    
+
+def validEmail(email):
+    user = database_helper.getUserByEmail(email)
+    if user is None:
+        return True
+    else:
+        return False
+
+@app.route('/')
+def main():
+    return "Hello"
 
 @app.route('/sign_in', methods=['POST'])
 def signIn():
     if validLogin(request.form['loginEmail'], request.form['loginPassword']):
         token = createToken()
-        return jsonify(success=True, message='Successfully signed in.', data=token)
+        result = database_helper.insertSignedInUser(token, request.form['loginEmail']);
+        if result == True:
+            return json.dumps({'success': True, 'message': 'Successfully signed in.', 'data': token}), 200
+        else:
+            return json.dumps({'success': False, 'message': 'Could not sign in user.'}), 501
     else:
-        return jsonify(success=False, message='Wrong username or password.')
+        return json.dumps({'success': False, 'message': 'Wrong username or password.'}), 404
+
+@app.route('/sign_up', methods=['POST'])
+def signUp():
+    form = SignUpForm(request.form)
+    if form.validate():
+        if validEmail(request.form['signupEmail']):
+            passwordHash = bcrypt.generate_password_hash(request.form['signupPassword'])
+            result = database_helper.insertUser(request.form['signupEmail'], request.form['firstName'], request.form['lastName'], request.form['gender'], request.form['city'], request.form['country'], passwordHash)
+            if result == True:            
+                return json.dumps({'success': True, 'message': 'Successfully created a new user.'}), 200
+            else:
+                return json.dumps({'success': False, 'message': 'Could not create user.'}), 501
+        else:
+            return json.dumps({'success': False, 'message': 'User already exists.'}), 404
+    else:
+        return json.dumps({'success': False, 'message': 'Form data missing or incorrect type.'}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
